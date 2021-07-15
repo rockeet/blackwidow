ec9e5a776e31e885c5b335004a190c492efd0f0a
leipeng <rockeet@gmail.com>
2021-07-15 18:58:47 +0800

    Revert "bw_side_plugin.cc: TTL_StreamReader: use env -i zstd ..."
    This reverts commit 00b4c468606d90837eec6cbb53dd87bbaad7e40d.

--- src/bw_side_plugin.cc
+++ src/bw_side_plugin.cc
@@ -211,7 +211,7 @@ struct TTL_StreamReader {
     char buf[32];
     fpath.append(buf, sprintf(buf, "/job-%08d/ttl", cp.job_id));
     //m_file.open(fpath, O_RDONLY, 0777);
-    m_file.open("env -i zstd -dcf " + fpath, "r");
+    m_file.open("zstd -dcf " + fpath, "r");
     m_reader.attach(&m_file);
     m_reader.set_bufsize(32*1024);
   }
