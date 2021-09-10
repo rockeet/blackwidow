63652a805f1da1ee1d7243ef3c6ceeb8ca979aa9
leipeng <rockeet@gmail.com>
2021-09-10 16:33:54 +0800

    ScopeSnapshot: disable snapshot on DBImplSecondary

---
+++ src/scope_snapshot.cc
@@ -0,0 +1,26 @@
+// created by leipeng 2021-09-10
+
+#include "scope_snapshot.h"
+#include <db/db_impl/db_impl_secondary.h>
+
+namespace blackwidow {
+
+ScopeSnapshot::ScopeSnapshot(rocksdb::DB* db, const rocksdb::Snapshot** snapshot)
+    : snapshot_(snapshot)
+{
+    if (dynamic_cast<rocksdb::DBImplSecondary*>(db)) {
+        // DBImplSecondary::NewIterator does not support snapshot
+        db_ = nullptr;
+        *snapshot_ = nullptr;
+    } else {
+        db_ = db;
+        *snapshot_ = db->GetSnapshot();
+    }
+}
+
+ScopeSnapshot::~ScopeSnapshot() {
+    if (db_)
+        db_->ReleaseSnapshot(*snapshot_);
+}
+
+} // namespace blackwidow
--- src/scope_snapshot.h
+++ src/scope_snapshot.h
@@ -11,18 +11,13 @@
 namespace blackwidow {
 class ScopeSnapshot {
  public:
-  ScopeSnapshot(rocksdb::DB* db, const rocksdb::Snapshot** snapshot) :
-    db_(db), snapshot_(snapshot) {
-    *snapshot_ = db_->GetSnapshot();
-  }
-  ~ScopeSnapshot() {
-    db_->ReleaseSnapshot(*snapshot_);
-  }
+  ScopeSnapshot(rocksdb::DB* db, const rocksdb::Snapshot** snapshot);
+  ~ScopeSnapshot();
  private:
-  rocksdb::DB* const db_;
+  rocksdb::DB* db_;
   const rocksdb::Snapshot** snapshot_;
-  ScopeSnapshot(const ScopeSnapshot&);
-  void operator=(const ScopeSnapshot&);
+  ScopeSnapshot(const ScopeSnapshot&) = delete;
+  void operator=(const ScopeSnapshot&) = delete;
 };
 
 }  // namespace blackwidow
